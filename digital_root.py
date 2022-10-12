6ef33c1ed4b1181ecbb55d911eeb06b10bf47f8c
akd75 <ak.drame@gmail.com>
2022-10-12 20:58:58 +0200

    ajout du fichier digital route level 6

---
+++ digital_root.py
@@ -0,0 +1,35 @@
+#DESCRIPTION:
+#Digital root is the recursive sum of all the digits in a number.
+
+#Given n, take the sum of the digits of n. If that value has more than one digit, 
+#continue reducing in this way until a single-digit number is produced. 
+#The input will be a non-negative integer.
+
+#Examples
+#    16  -->  1 + 6 = 7
+#   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+#132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+#493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 =
+
+def digital_root(n):
+    res=[]
+    final=[]
+    
+    to_str=str(n)
+    if len(to_str)<=1:
+        return n
+    else:        
+        while len(to_str)>1:
+            to_str=str(to_str)
+            res=[]
+            final=[]
+            for i in to_str:
+                to_string=int(i)
+                res.append(to_string)
+            final=sum(res)
+            to_str=str(final)
+            print(n)        
+            print(res)
+            print(to_str)
+
+    return final
